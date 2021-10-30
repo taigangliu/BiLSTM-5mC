7a96b8933d7ee8af6e657bd903f9b17695b14c9a
taigangliu <44895765+taigangliu@users.noreply.github.com>
2021-10-30 12:35:54 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+train_data_path = os.getcwd()+os.sep+'dataset'+os.sep+'data.txt'
+dev_file = ""
+test_file = ""
+train_batch_size=32
+eval_batch_size = 32
+test_batch_size = 32
+max_seq_length = 1500
